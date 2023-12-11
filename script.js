07bed497f814d0e8f21a614cb1b6dadff70d22f0
Steelbulloff <Steelbulloff@gmail.com>
2023-12-11 23:48:46 +0600

    aded confirm button

--- script.js
+++ script.js
@@ -6,10 +6,33 @@ let addTodoButton = document.querySelector(".add")
 let areaTodo = document.querySelector(".todoarea")
 //кнопка удалить
 let deleteTodoButton = document.querySelector(".delete")
+//кнопка конфирм
+let confirmButton = document.querySelector(".confirm")
 //Клик добавить
 addTodoButton.addEventListener('click',addNewTodo)
 //клик удалить
 deleteTodoButton.addEventListener('click',deleteTodos)
+//клик завершить
+confirmButton.addEventListener('click',confirmTodo)
+
+function confirmTodo(){
+    //див внутри дива
+    let todoelements = document.querySelectorAll(".todoelement")
+    //чекбокс
+    let checkboxToDelete = document.querySelectorAll(".checkmeout")
+    checkboxToDelete.forEach(element => {
+        console.log(element.checked)
+        if (element.checked){
+            element.checked = !element.checked;
+            if(element.parentNode.style.textDecoration == "line-through"){
+                element.parentNode.style.textDecoration = "none" 
+            }else{
+                element.parentNode.style.textDecoration = "line-through";
+            }
+        }
+        
+    });
+}
 //функ удаления 
 function deleteTodos(){
     //див внутри дива
@@ -19,7 +42,7 @@ function deleteTodos(){
     checkboxToDelete.forEach(element => {
         console.log(element.checked)
         if (element.checked){
-            element.parentNode.parentNode.removeChild(element.parentNode);
+            element.parentNode.parentNode.removeChild(element.parentNode);      
         }
         
     });
